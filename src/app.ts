a31d4c5302daf0ca5a21d6b3f2820529e19c8a28
khewdev <khewdev@gmail.com>
2021-07-21 00:23:30 +0800

    added mongoose and connected to db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import config from "config"
 import log from './logger'
+import connect from "./db/connect"
+import routes from './routes'
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -12,4 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
     log.info(`Server listening at http://${host}:${port}`);
+    connect();
+    routes(app);
 })
